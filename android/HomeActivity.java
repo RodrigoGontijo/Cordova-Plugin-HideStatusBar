40a7e6b808d70c05db4818075cf661e061cc3239
Rodrigo Gontijo <rodrigo.gontijo@hotmail.com>
2016-06-24 15:46:49 -0300

    test

--- android/HomeActivity.java
+++ android/HomeActivity.java
@@ -10,43 +10,57 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.KeyEvent;
 import android.view.ViewGroup.LayoutParams;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class HomeActivity extends Activity {
-
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        HomeActivity.this.startKioskActivity();
-
-
+        
+        LinearLayout layout = new LinearLayout(this);
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        
+        Button button = new Button(this);
+        button.setText("Click or press any key to begin...");
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                HomeActivity.this.startKioskActivity();
+            }
+        });
+        layout.addView(button, params);
+        
+        setContentView(layout);
     }
-
+    
     @Override
     protected void onResume() {
-
-        HomeActivity.this.startKioskActivity();
-
+        super.onResume();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask(){
+            public void run() {
+                HomeActivity.this.startKioskActivity();
+            }
+        }, 20000); // 20 seconds
     }
-
+    
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+        startKioskActivity();
         return true; // prevent event from being propagated
     }
-
+    
     // http://www.andreas-schrade.de/2015/02/16/android-tutorial-how-to-create-a-kiosk-mode-in-android/
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if (!hasFocus) {
+        if(!hasFocus) {
             Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
             sendBroadcast(closeDialog);
-
+            
             // sametime required to close opened notification area
             Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
+            timer.schedule(new TimerTask(){
                 public void run() {
                     Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
                     sendBroadcast(closeDialog);
@@ -54,7 +68,7 @@ public class HomeActivity extends Activity {
             }, 500); // 0.5 second
         }
     }
-
+    
     private void startKioskActivity() {
         Intent serviceIntent = new Intent(this, KioskActivity.class);
         startActivity(serviceIntent);
